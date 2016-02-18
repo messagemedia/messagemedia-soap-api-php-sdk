027e0f420a94afafcf41373b75deb6c85248bfe4
Ruwen Schwedewsky <Ruwen.Schwedewsky@messagemedia.com.au>
2016-02-18 13:54:12 +1100

    ASIT-57: correct year in the copyright

--- examples_account_details.php
+++ examples_account_details.php
@@ -2,7 +2,7 @@
 /**
  * PHP MMSoap Example code around account details
  *
- * Copyright 2014 MessageMedia
+ * Copyright 2016 MessageMedia
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
