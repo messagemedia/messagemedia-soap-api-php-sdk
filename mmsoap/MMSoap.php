dda504361845f31f834188f4f38eec7eb95be9a7
Ruwen Schwedewsky <Ruwen.Schwedewsky@messagemedia.com.au>
2016-02-23 11:28:09 +1100

    fix copyright year

--- mmsoap/MMSoap.php
+++ mmsoap/MMSoap.php
@@ -2,7 +2,7 @@
 /**
  * MMSoap class for interactive with MessageMedia's SOAP API
  *
- * Copyright 2016 MessageMedia
+ * Copyright 2014-2016 MessageMedia
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License.
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
